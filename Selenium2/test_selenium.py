b77122ffb18ad4ad2b01010706a5ace564eb2122
Maria_La <160595950+Dreammarya@users.noreply.github.com>
2025-04-18 23:55:47 +0200

    Update test_selenium.py

--- Selenium2/test_selenium.py
+++ Selenium2/test_selenium.py
@@ -4,14 +4,14 @@ import time  # Import the time module for adding delays
 # Instantiate the web driver for Chrome
 driver = webdriver.Chrome()
 
-# Navigate to the Masterschool website
+# Navigate to website
 driver.get("https://www.masterschool.com")
 
-# Wait for 3 seconds to allow the page to load completely
+# Wait for 3 sec
 time.sleep(3)
 
 # Print the title of the page to the console
 print(driver.title)
 
-# Close the browser and end the WebDriver session
+# Close the  WebDriver session
 driver.quit()
